17588ee6f0d88f02571498bfc58924c7c615c874
zarkoc7 <zarko.rm7@gmail.com>
2023-07-01 19:56:01 +0200

    Kreirana logika postova na drustvenoj mrezi

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ import {register} from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import { verifyToken } from "./middleware/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { createPost } from "./controllers/posts.js";
 
 /* Configurations */
 
@@ -42,11 +44,14 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), verifyToken, register);
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
